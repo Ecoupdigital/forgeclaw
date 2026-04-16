5f54ccd09eabb79755d80885783906a403ec85c6
Jonathan Renan <sistemas@ecoupdigital.com.br>
2026-04-16 22:53:26 +0000

    feat(21-05): wire TokensTab and ActivityTab into page.tsx
    - Import and instantiate TokensTab and ActivityTab components
    - Pass as props to DashboardShell (8 tabs total)

--- packages/dashboard/src/app/page.tsx
+++ packages/dashboard/src/app/page.tsx
@@ -4,6 +4,9 @@ import { CronsTab } from "@/components/crons-tab";
 import { MemoryTab } from "@/components/memory-tab";
 import { ConfigTab } from "@/components/config-tab";
 import { HarnessTab } from "@/components/harness-tab";
+import { WebhooksTab } from "@/components/webhooks-tab";
+import { TokensTab } from "@/components/tokens-tab";
+import { ActivityTab } from "@/components/activity-tab";
 
 export default function HomePage() {
   return (
@@ -13,6 +16,9 @@ export default function HomePage() {
       memoryTab={<MemoryTab />}
       configTab={<ConfigTab />}
       harnessTab={<HarnessTab />}
+      webhooksTab={<WebhooksTab />}
+      tokensTab={<TokensTab />}
+      activityTab={<ActivityTab />}
     />
   );
 }
